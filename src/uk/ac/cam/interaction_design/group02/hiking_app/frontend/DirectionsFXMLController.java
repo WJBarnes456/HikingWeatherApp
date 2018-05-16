6b005391dfba44996b72399b335c107d1028fcdc
pawelburzynski98 <emajlpawla@gmail.com>
2018-05-17 00:53:33 +0100

    maps

--- src/uk/ac/cam/interaction_design/group02/hiking_app/frontend/DirectionsFXMLController.java
+++ src/uk/ac/cam/interaction_design/group02/hiking_app/frontend/DirectionsFXMLController.java
@@ -2,77 +2,93 @@ package uk.ac.cam.interaction_design.group02.hiking_app.frontend;
 
 import com.lynden.gmapsfx.GoogleMapView;
 import com.lynden.gmapsfx.MapComponentInitializedListener;
-import com.lynden.gmapsfx.javascript.object.*;
-import com.lynden.gmapsfx.service.directions.*;
+import com.lynden.gmapsfx.javascript.object.GoogleMap;
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import com.lynden.gmapsfx.javascript.object.MapOptions;
+import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
+import com.lynden.gmapsfx.javascript.object.Marker;
+import com.lynden.gmapsfx.javascript.object.MarkerOptions;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
 import uk.ac.cam.interaction_design.group02.hiking_app.backend.APIKey;
 
-public class DirectionsFXMLController implements Initializable, MapComponentInitializedListener, DirectionsServiceCallback {
-
-    protected DirectionsService directionsService;
-    protected DirectionsPane directionsPane;
-
-    protected StringProperty from = new SimpleStringProperty();
-    protected StringProperty to = new SimpleStringProperty();
-    protected DirectionsRenderer directionsRenderer = null;
+public class DirectionsFXMLController implements Initializable, MapComponentInitializedListener {
 
     @FXML
-    protected BorderPane mapViewHolder;
-
-    protected GoogleMapView mapView;
+    private Button button;
 
     @FXML
-    protected TextField fromTextField;
+    private GoogleMapView mapView ;
 
-    @FXML
-    protected TextField toTextField;
-
-    @FXML
-    private void toTextFieldAction(ActionEvent event) {
-        DirectionsRequest request = new DirectionsRequest(from.get(), to.get(), TravelModes.DRIVING);
-        directionsRenderer = new DirectionsRenderer(true, mapView.getMap(), directionsPane);
-        directionsService.getRoute(request, this, directionsRenderer);
-    }
-
-    @FXML
-    private void clearDirections(ActionEvent event) {
-        directionsRenderer.clearDirections();
-    }
-
-    @Override
-    public void directionsReceived(DirectionsResult results, DirectionStatus status) {
-    }
+    private GoogleMap map;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         mapView = new GoogleMapView(null, APIKey.getGoogleMapsKey());
-        mapViewHolder.setCenter(mapView);
-
         mapView.addMapInializedListener(this);
-        to.bindBidirectional(toTextField.textProperty());
-        from.bindBidirectional(fromTextField.textProperty());
     }
 
     @Override
     public void mapInitialized() {
-        MapOptions options = new MapOptions();
+        LatLong joeSmithLocation = new LatLong(47.6197, -122.3231);
+        LatLong joshAndersonLocation = new LatLong(47.6297, -122.3431);
+        LatLong bobUnderwoodLocation = new LatLong(47.6397, -122.3031);
+        LatLong tomChoiceLocation = new LatLong(47.6497, -122.3325);
+        LatLong fredWilkieLocation = new LatLong(47.6597, -122.3357);
 
-        options.center(new LatLong(47.606189, -122.335842))
-                .zoomControl(true)
-                .zoom(12)
+
+        //Set the initial properties of the map.
+        MapOptions mapOptions = new MapOptions();
+
+        mapOptions.center(new LatLong(47.6097, -122.3331))
+                .mapType(MapTypeIdEnum.ROADMAP)
                 .overviewMapControl(false)
-                .mapType(MapTypeIdEnum.ROADMAP);
-        GoogleMap map = mapView.createMap(options);
-        directionsService = new DirectionsService();
-        directionsPane = mapView.getDirec();
-    }
+                .panControl(false)
+                .rotateControl(false)
+                .scaleControl(false)
+                .streetViewControl(false)
+                .zoomControl(false)
+                .zoom(12);
 
+        map = mapView.createMap(mapOptions);
+
+        //Add markers to the map
+        MarkerOptions markerOptions1 = new MarkerOptions();
+        markerOptions1.position(joeSmithLocation);
+
+        MarkerOptions markerOptions2 = new MarkerOptions();
+        markerOptions2.position(joshAndersonLocation);
+
+        MarkerOptions markerOptions3 = new MarkerOptions();
+        markerOptions3.position(bobUnderwoodLocation);
+
+        MarkerOptions markerOptions4 = new MarkerOptions();
+        markerOptions4.position(tomChoiceLocation);
+
+        MarkerOptions markerOptions5 = new MarkerOptions();
+        markerOptions5.position(fredWilkieLocation);
+
+        Marker joeSmithMarker = new Marker(markerOptions1);
+        Marker joshAndersonMarker = new Marker(markerOptions2);
+        Marker bobUnderwoodMarker = new Marker(markerOptions3);
+        Marker tomChoiceMarker= new Marker(markerOptions4);
+        Marker fredWilkieMarker = new Marker(markerOptions5);
+
+        map.addMarker( joeSmithMarker );
+        map.addMarker( joshAndersonMarker );
+        map.addMarker( bobUnderwoodMarker );
+        map.addMarker( tomChoiceMarker );
+        map.addMarker( fredWilkieMarker );
+
+     //   InfoWindowOptions infoWindowOptions = new InfoWindowOptions();
+     //   infoWindowOptions.content("<h2>Fred Wilkie</h2>"
+      //          + "Current Location: Safeway<br>"
+      //          + "ETA: 45 minutes" );
+
+    //    InfoWindow fredWilkeInfoWindow = new InfoWindow(infoWindowOptions);
+     //   fredWilkeInfoWindow.open(map, fredWilkieMarker);
+    }
 }

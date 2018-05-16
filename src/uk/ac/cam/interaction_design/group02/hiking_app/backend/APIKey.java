8296338ff1e4544d69d6b682ac19c38812c20470
Will Barnes <39161512+WJBarnes456@users.noreply.github.com>
2018-05-16 21:13:55 +0100

    Update to proper Eclipse project
    The dependencies are now stored in ClassPath. The directory now has a proper iml file in it, for those of us using IntelliJ
    
    I also undid my dumb commit of the API key. Lesson learned - git ignore it straight away next time.

--- src/uk/ac/cam/interaction_design/group02/hiking_app/backend/APIKey.java
+++ src/uk/ac/cam/interaction_design/group02/hiking_app/backend/APIKey.java
@@ -1,7 +1,7 @@
 package uk.ac.cam.interaction_design.group02.hiking_app.backend;
 
 public class APIKey {
-    private static final String Key = "4633a32f54c741d3a821ac6c016ccf1d"; //Don't ever commit this.
+    private static final String Key = "YOUR_SECRET_KEY_HERE"; //Don't ever commit this.
     
     public static String getKey() {
         return Key;

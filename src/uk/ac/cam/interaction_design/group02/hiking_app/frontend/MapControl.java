8edbcf7d369e4484b6da2ca81844267ab3083f17
Will Barnes <39161512+WJBarnes456@users.noreply.github.com>
2018-05-17 15:16:22 +0100

    Merge map controls into MapControl

--- src/uk/ac/cam/interaction_design/group02/hiking_app/frontend/MapControl.java
+++ src/uk/ac/cam/interaction_design/group02/hiking_app/frontend/MapControl.java
@@ -1,22 +1,103 @@
 package uk.ac.cam.interaction_design.group02.hiking_app.frontend;
 
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.*;
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import uk.ac.cam.interaction_design.group02.hiking_app.backend.APIKey;
 
 import java.io.IOException;
 
-public class MapControl extends AnchorPane {
+public class MapControl extends AnchorPane implements MapComponentInitializedListener {
+    @FXML
+    private BorderPane mapViewHolder;
+
+    private GoogleMapView mapView;
+
+    private GoogleMap map;
+
+    @FXML
+    public void initialize() {
+        mapView = new GoogleMapView(null, APIKey.getGoogleMapsKey());
+        mapView.addMapInializedListener(this);
+    }
+
+
     public MapControl() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MapControl.fxml"));
         fxmlLoader.setRoot(this); //Required since this is loading a component
-        // fxmlLoader.setController(this); //Commented out while we're temporarily delegating to ext. controller
+        fxmlLoader.setController(this); //Commented out while we're temporarily delegating to ext. controller
         //Controller HAS to be set here, otherwise you get a self-cyclic instantiation => VERY BAD
-
         fxmlLoader.load();
+    }
+
+    @Override
+    public void mapInitialized() {
+        mapViewHolder.setCenter(mapView);
+        LatLong joeSmithLocation = new LatLong(47.6197, -122.3231);
+        LatLong joshAndersonLocation = new LatLong(47.6297, -122.3431);
+        LatLong bobUnderwoodLocation = new LatLong(47.6397, -122.3031);
+        LatLong tomChoiceLocation = new LatLong(47.6497, -122.3325);
+        LatLong fredWilkieLocation = new LatLong(47.6597, -122.3357);
+
+
+        //Set the initial properties of the map.
+        MapOptions mapOptions = new MapOptions();
+
+        mapOptions.center(new LatLong(47.6097, -122.3331))
+                .mapType(MapTypeIdEnum.ROADMAP)
+                .overviewMapControl(false)
+                .panControl(false)
+                .rotateControl(false)
+                .scaleControl(false)
+                .streetViewControl(false)
+                .zoomControl(false)
+                .zoom(12);
+
+        map = mapView.createMap(mapOptions);
+
+        //Add markers to the map
+        MarkerOptions markerOptions1 = new MarkerOptions();
+        markerOptions1.position(joeSmithLocation);
+
+        MarkerOptions markerOptions2 = new MarkerOptions();
+        markerOptions2.position(joshAndersonLocation);
+
+        MarkerOptions markerOptions3 = new MarkerOptions();
+        markerOptions3.position(bobUnderwoodLocation);
+
+        MarkerOptions markerOptions4 = new MarkerOptions();
+        markerOptions4.position(tomChoiceLocation);
+
+        MarkerOptions markerOptions5 = new MarkerOptions();
+        markerOptions5.position(fredWilkieLocation);
+
+        Marker joeSmithMarker = new Marker(markerOptions1);
+        Marker joshAndersonMarker = new Marker(markerOptions2);
+        Marker bobUnderwoodMarker = new Marker(markerOptions3);
+        Marker tomChoiceMarker= new Marker(markerOptions4);
+        Marker fredWilkieMarker = new Marker(markerOptions5);
+
+        map.addMarker( joeSmithMarker );
+        map.addMarker( joshAndersonMarker );
+        map.addMarker( bobUnderwoodMarker );
+        map.addMarker( tomChoiceMarker );
+        map.addMarker( fredWilkieMarker );
+
+        //   InfoWindowOptions infoWindowOptions = new InfoWindowOptions();
+        //   infoWindowOptions.content("<h2>Fred Wilkie</h2>"
+        //          + "Current Location: Safeway<br>"
+        //          + "ETA: 45 minutes" );
+
+        //    InfoWindow fredWilkeInfoWindow = new InfoWindow(infoWindowOptions);
+        //   fredWilkeInfoWindow.open(map, fredWilkieMarker);
     }
 }

252a5767152b4696e4a3a391e6d0365e9bf11fbd
Will Barnes <39161512+WJBarnes456@users.noreply.github.com>
2018-05-19 16:04:01 +0100

    Make DetailsControl compile
    Errors fixed:
     - Mismatched typing (double -> String)
     - Added hike as a property
     - Imported NaiveAPI and used the correct call
    
    Stylistic problems fixed:
     - Changed from tabs to 4 spaces (THIS IS OUR CONVENTION)
     - Put @FXML annotations on the preceding line - easier to read.

--- src/uk/ac/cam/interaction_design/group02/hiking_app/frontend/DetailsControl.java
+++ src/uk/ac/cam/interaction_design/group02/hiking_app/frontend/DetailsControl.java
@@ -1,10 +1,9 @@
 package uk.ac.cam.interaction_design.group02.hiking_app.frontend;
 
 import java.io.IOException;
-import uk.ac.cam.interaction_design.group02.hiking_app.backend.Hike;
-import uk.ac.cam.interaction_design.group02.hiking_app.backend.ForecastWeatherPoint;
-import uk.ac.cam.interaction_design.group02.hiking_app.backend.NaiveAPI;
-import uk.ac.cam.interaction_design.group02.hiking_app.backend.WeatherData;
+
+import uk.ac.cam.interaction_design.group02.hiking_app.backend.*;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,46 +14,68 @@ import javafx.scene.control.Label;
 
 import javafx.fxml.FXMLLoader;
 
-public class DetailsControl implements Initializable {
+public class DetailsControl {
 	
 	//Use the fxml fx:id tag as the name of the object
 	// You can find these in the fxml file, it is normally the first attribute.
 	// The name has to be exactly the same as the fx:id in the fxml file
-	@FXML Label humidity;
-	@FXML Label visibility;
-	@FXML Label air_pressure;
-	@FXML Label high;
-	@FXML Label probability;
-	@FXML Label intensity
+	@FXML
+	Label humidity;
+
+	@FXML
+	Label visibility;
+
+	@FXML
+	Label air_pressure;
+
+	@FXML
+	Label high;
+
+	@FXML
+    Label probability;
+
+	@FXML
+    Label intensity;
+
+	Hike hike;
+
 	
 	@FXML
-    	public void initialize() {
-		
+    public void initialize() throws APIException, ForecastException {
+	    IAPICache api = new NaiveAPI();
+
 		double latitude = hike.getLatitude();
 		double longitude = hike.getLongitude();
 		long startTime = hike.getStartTime();
-		
-		ForecastWeatherPoint weatherPoint = fetchWeatherUsingAPI(latitude,longitude);
-		WeatherData data = weatherPoint.getForecastAtTime(startTime);
-		
-		humidity.setText(data.getHumidity());
-		visibility.setText(data.getVisibility());
-		air_pressure.setText(data.getPressure());
-		high.setText(data.getHighTemperatureCelsius());
-		probability.setText(data.getPrecipitationProbability());
-		intensity.setText(data.getPrecipitationIntensity());
 
-    	}
+		ForecastWeatherPoint weatherPoint = api.getWeatherForPoint(latitude,longitude);
+		WeatherData data = weatherPoint.getForecastAtTime(startTime);
+
+		String humidityString = Double.toString(data.getHumidity());
+        String visibilityString = Double.toString(data.getVisibility());
+        String pressureString = Double.toString(data.getPressure());
+        String highTempString = Double.toString(data.getHighTemperatureCelsius());
+        String precipProbString = Double.toString(data.getPrecipitationProbability());
+        String precipIntensityString = Double.toString(data.getPrecipitationIntensity());
+
+		humidity.setText(humidityString);
+		visibility.setText(visibilityString);
+		air_pressure.setText(pressureString);
+		high.setText(highTempString);
+		probability.setText(precipProbString);
+		intensity.setText(precipIntensityString);
+	}
 	
 	public DetailsControl(Hike hike) throws IOException {
-		
-        	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DetailsControl.fxml"));
-        	fxmlLoader.setRoot(this);
-        	fxmlLoader.setController(this);
-        	//Controller HAS to be set here, otherwise you get a self-cyclic instantiation => VERY BAD
+	    this.hike = hike;
 
-        	fxmlLoader.load();
-    	}
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DetailsControl.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        //Controller HAS to be set here, otherwise you get a self-cyclic instantiation => VERY BAD
+
+        fxmlLoader.load();
+    }
 	
 	
 	

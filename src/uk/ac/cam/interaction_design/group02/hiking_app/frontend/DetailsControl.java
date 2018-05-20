eae0cd82b501fd5e0e5a553ada2c5a05b7a8f23a
Will Barnes <39161512+WJBarnes456@users.noreply.github.com>
2018-05-20 17:19:51 +0100

    Add icons to WeatherData
    Also did formatting on DetailsControl

--- src/uk/ac/cam/interaction_design/group02/hiking_app/frontend/DetailsControl.java
+++ src/uk/ac/cam/interaction_design/group02/hiking_app/frontend/DetailsControl.java
@@ -18,59 +18,59 @@ import javafx.scene.control.Label;
 import javafx.fxml.FXMLLoader;
 
 public class DetailsControl extends Pane {
-	
-	//Use the fxml fx:id tag as the name of the object
-	// You can find these in the fxml file, it is normally the first attribute.
-	// The name has to be exactly the same as the fx:id in the fxml file
-	@FXML
-	Label humidity;
 
-	@FXML
-	Label visibility;
+    //Use the fxml fx:id tag as the name of the object
+    // You can find these in the fxml file, it is normally the first attribute.
+    // The name has to be exactly the same as the fx:id in the fxml file
+    @FXML
+    Label humidity;
 
-	@FXML
-	Label air_pressure;
+    @FXML
+    Label visibility;
 
-	@FXML
-	Label high;
+    @FXML
+    Label air_pressure;
 
-	@FXML
+    @FXML
+    Label high;
+
+    @FXML
     Label probability;
 
-	@FXML
+    @FXML
     Label intensity;
 
-	Hike hike;
+    Hike hike;
 
-	
-	@FXML
+
+    @FXML
     public void initialize() throws APIException, ForecastException {
-	    IAPICache api = NaiveAPI.getInstance();
+        IAPICache api = NaiveAPI.getInstance();
 
-		double latitude = hike.getLatitude();
-		double longitude = hike.getLongitude();
-		long startTime = hike.getStartTime();
+        double latitude = hike.getLatitude();
+        double longitude = hike.getLongitude();
+        long startTime = hike.getStartTime();
 
-		ForecastWeatherPoint weatherPoint = api.getWeatherForPoint(latitude,longitude);
-		WeatherData data = weatherPoint.getForecastAtTime(startTime);
+        ForecastWeatherPoint weatherPoint = api.getWeatherForPoint(latitude, longitude);
+        WeatherData data = weatherPoint.getForecastAtTime(startTime);
 
-		String humidityString = Double.toString(data.getHumidity());
+        String humidityString = Double.toString(data.getHumidity());
         String visibilityString = Double.toString(data.getVisibility());
         String pressureString = Double.toString(data.getPressure());
         String highTempString = Double.toString(data.getHighTemperatureCelsius());
         String precipProbString = Double.toString(data.getPrecipitationProbability());
         String precipIntensityString = Double.toString(data.getPrecipitationIntensity());
 
-		humidity.setText(humidityString);
-		visibility.setText(visibilityString);
-		air_pressure.setText(pressureString);
-		high.setText(highTempString);
-		probability.setText(precipProbString);
-		intensity.setText(precipIntensityString);
-	}
-	
-	public DetailsControl(Hike hike) throws IOException {
-	    this.hike = hike;
+        humidity.setText(humidityString);
+        visibility.setText(visibilityString);
+        air_pressure.setText(pressureString);
+        high.setText(highTempString);
+        probability.setText(precipProbString);
+        intensity.setText(precipIntensityString);
+    }
+
+    public DetailsControl(Hike hike) throws IOException {
+        this.hike = hike;
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DetailsControl.fxml"));
         fxmlLoader.setRoot(this);
@@ -82,11 +82,12 @@ public class DetailsControl extends Pane {
 
     /**
      * Helper method used to easily show a dialog for a hike
+     *
      * @param hike The hike to show details for
      */
     public static void showDetailsPopup(Hike hike) throws IOException {
-		Stage detailsStage = new Stage();
-		detailsStage.setScene(new Scene(new DetailsControl(hike)));
-		detailsStage.showAndWait();
-	}
+        Stage detailsStage = new Stage();
+        detailsStage.setScene(new Scene(new DetailsControl(hike)));
+        detailsStage.showAndWait();
+    }
 }

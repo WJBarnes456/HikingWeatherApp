51ef9e54e78ad1482696b58855188488ceecc8d0
Will Barnes <39161512+WJBarnes456@users.noreply.github.com>
2018-05-20 17:36:25 +0100

    Improve design of MapControl

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HikingWeatherApp
 Hi-Fi Prototype for University of Cambridge Interaction Design 2017-18, Group 02
+
+Setup instructions:
+ - Duplicate APIKey_Example, rename it to APIKey, and put your key files into.
--- src/uk/ac/cam/interaction_design/group02/hiking_app/frontend/MapControl.java
+++ src/uk/ac/cam/interaction_design/group02/hiking_app/frontend/MapControl.java
@@ -7,11 +7,11 @@ import com.lynden.gmapsfx.javascript.event.UIEventType;
 import com.lynden.gmapsfx.javascript.object.*;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import netscape.javascript.JSObject;
 import uk.ac.cam.interaction_design.group02.hiking_app.backend.APIKey;
 import uk.ac.cam.interaction_design.group02.hiking_app.backend.AppSettings;
@@ -19,6 +19,7 @@ import uk.ac.cam.interaction_design.group02.hiking_app.backend.Hike;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class MapControl extends BorderPane implements MapComponentInitializedListener {
@@ -38,18 +39,48 @@ public class MapControl extends BorderPane implements MapComponentInitializedLis
 
     @FXML
     private Button todayButton;
+    @FXML
+    private Label todayButtonMonth;
+    @FXML
+    private Label todayButtonDate;
+    @FXML
+    private Label todayButtonDayOfWeek;
 
     @FXML
     private Button tomorrowButton;
+    @FXML
+    private Label tomorrowButtonMonth;
+    @FXML
+    private Label tomorrowButtonDate;
+    @FXML
+    private Label tomorrowButtonDayOfWeek;
 
     @FXML
     private Button twoDaysButton;
+    @FXML
+    private Label twoDaysButtonMonth;
+    @FXML
+    private Label twoDaysButtonDate;
+    @FXML
+    private Label twoDaysButtonDayOfWeek;
 
     @FXML
     private Button threeDaysButton;
+    @FXML
+    private Label threeDaysButtonMonth;
+    @FXML
+    private Label threeDaysButtonDate;
+    @FXML
+    private Label threeDaysButtonDayOfWeek;
 
     @FXML
     private Button fourDaysButton;
+    @FXML
+    private Label fourDaysButtonMonth;
+    @FXML
+    private Label fourDaysButtonDate;
+    @FXML
+    private Label fourDaysButtonDayOfWeek;
 
     @FXML
     private Button laterButton;
@@ -61,8 +92,7 @@ public class MapControl extends BorderPane implements MapComponentInitializedLis
     }
 
     private void markUnset(Button b) {
-        b.setStyle("-fx-background-color: #286d51;" +
-                "-fx-text-fill: #c5d9d1");
+        b.setStyle("-fx-background-color: linear-gradient(to bottom, #3b9769, #258153 10%, #1b6749 20%, #0b5739 90%)");
     }
 
     private void markAllUnset() {
@@ -71,7 +101,6 @@ public class MapControl extends BorderPane implements MapComponentInitializedLis
         markUnset(twoDaysButton);
         markUnset(threeDaysButton);
         markUnset(fourDaysButton);
-        markUnset(laterButton);
     }
 
     @FXML
@@ -79,7 +108,7 @@ public class MapControl extends BorderPane implements MapComponentInitializedLis
         mapView = new GoogleMapView(null, APIKey.getGoogleMapsKey());
         mapView.addMapInializedListener(this);
 
-        markAllUnset();
+        initializeButtons();
     }
 
 
@@ -126,7 +155,7 @@ public class MapControl extends BorderPane implements MapComponentInitializedLis
 
         InfoWindowOptions infoWindowOptions = new InfoWindowOptions();
         infoWindowOptions.content("<h2>Location clicked</h2> " +
-                        "Lat: " + latLong.getLatitude() + "<br>" +
+                "Lat: " + latLong.getLatitude() + "<br>" +
                 "Long: " + latLong.getLongitude());
         // TODO: Implement hike addition, getting weather for this clicked point
 
@@ -147,20 +176,14 @@ public class MapControl extends BorderPane implements MapComponentInitializedLis
      * @param hikeMarker The marker of the hike to show information on
      * @param hike The raw hike object to show information on
      */
-    private void handleHikeMarkerClick(Marker hikeMarker, Hike hike) {
+    private void handleMarkerClick(Marker hikeMarker, Hike hike) {
         cleanMarker();
+        InfoWindowOptions infoWindowOptions = new InfoWindowOptions();
+        infoWindowOptions.content("<h2>"+hike.getName()+"</h2>");
+        // TODO: Show weather about a location when the hike is clicked
 
-        try {
-            DetailsControl.showDetailsPopup(hike);
-        }
-        catch (IOException e) {
-            // Fallback if DetailsControl throws an IOException and can't load
-            InfoWindowOptions infoWindowOptions = new InfoWindowOptions();
-            infoWindowOptions.content("<h2>"+hike.getName()+"</h2>");
-
-            clickInfoWindow = new InfoWindow(infoWindowOptions);
-            clickInfoWindow.open(map, hikeMarker);
-        }
+        clickInfoWindow = new InfoWindow(infoWindowOptions);
+        clickInfoWindow.open(map, hikeMarker);
     }
 
     /**
@@ -199,7 +222,7 @@ public class MapControl extends BorderPane implements MapComponentInitializedLis
                 Marker hikeMarker = new Marker(hikeOptions);
 
                 map.addUIEventHandler(hikeMarker, UIEventType.click, (JSObject j) -> {
-                    handleHikeMarkerClick(hikeMarker, h);
+                    handleMarkerClick(hikeMarker, h);
                 });
 
                 hikeMarkers.add(hikeMarker);
@@ -207,5 +230,48 @@ public class MapControl extends BorderPane implements MapComponentInitializedLis
                 map.addMarker(hikeMarker);
             }
         }
+    }
+
+    private void initializeButtons() {
+        // stores the dates for five consecutive days, starting from the current date, in the format MMM dd E
+        String[][] dateParameters = new String[5][3];
+
+        // initializes the calendar with the current date
+        Calendar calendar = Calendar.getInstance();
+        String[] date = calendar.getTime().toString().split(" ");
+        dateParameters[0][0] = date[1];
+        dateParameters[0][1] = date[2];
+        dateParameters[0][2] = date[0];
+
+        // adds the following dates from 1 day later to 4 days later (inclusive)
+        for (int i = 1; i <= 4; i++) {
+            calendar.add(Calendar.DATE, 1);
+            date = calendar.getTime().toString().split(" ");
+            dateParameters[i][0] = date[1];
+            dateParameters[i][1] = date[2];
+            dateParameters[i][2] = date[0];
+        }
+
+        // initializes the five buttons to display the five dates
+        todayButtonMonth.setText(dateParameters[0][0]);
+        todayButtonDate.setText(dateParameters[0][1]);
+        todayButtonDayOfWeek.setText(dateParameters[0][2]);
+
+        tomorrowButtonMonth.setText(dateParameters[1][0]);
+        tomorrowButtonDate.setText(dateParameters[1][1]);
+        tomorrowButtonDayOfWeek.setText(dateParameters[1][2]);
+
+        twoDaysButtonMonth.setText(dateParameters[2][0]);
+        twoDaysButtonDate.setText(dateParameters[2][1]);
+        twoDaysButtonDayOfWeek.setText(dateParameters[2][2]);
+
+        threeDaysButtonMonth.setText(dateParameters[3][0]);
+        threeDaysButtonDate.setText(dateParameters[3][1]);
+        threeDaysButtonDayOfWeek.setText(dateParameters[3][2]);
+
+        fourDaysButtonMonth.setText(dateParameters[4][0]);
+        fourDaysButtonDate.setText(dateParameters[4][1]);
+        fourDaysButtonDayOfWeek.setText(dateParameters[4][2]);
+
     }
 }

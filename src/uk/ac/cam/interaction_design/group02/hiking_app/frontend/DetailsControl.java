891bdaf9673bd5c78290cc0354ea0083c08bdd2f
Will Barnes <39161512+WJBarnes456@users.noreply.github.com>
2018-05-21 11:59:59 +0100

    Get graph working in DetailsControl
    Made a couple of other little changes too

--- src/uk/ac/cam/interaction_design/group02/hiking_app/frontend/DetailsControl.java
+++ src/uk/ac/cam/interaction_design/group02/hiking_app/frontend/DetailsControl.java
@@ -7,7 +7,7 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.util.StringConverter;
 import uk.ac.cam.interaction_design.group02.hiking_app.backend.*;
@@ -38,7 +38,7 @@ public class DetailsControl extends GridPane {
     Label air_pressure;
 
     @FXML
-    Label high;
+    Label temperature;
 
     @FXML
     Label probability;
@@ -50,12 +50,15 @@ public class DetailsControl extends GridPane {
     LineChart<Number, Number> linechart;
 
     @FXML
+    Label hikeName;
+
+    @FXML
     NumberAxis xAxis;
 
     Hike hike;
 
     @FXML
-    public void initialize() throws APIException, ForecastException {
+    public void initialize() throws APIException {
         IAPICache api = NaiveAPI.getInstance();
 
         double latitude = hike.getLatitude();
@@ -63,63 +66,103 @@ public class DetailsControl extends GridPane {
         long startTime = hike.getStartTime();
         long endTime = hike.getEndTime();
 
+        hikeName.setText(hike.getName());
+
         // Get weather information
         ForecastWeatherPoint weatherPoint = api.getWeatherForPoint(latitude, longitude);
-        WeatherData data = weatherPoint.getForecastAtTime(startTime);
 
+        try {
+            WeatherData data = weatherPoint.getForecastAtTime(startTime);
+
+            setWeatherLabels(data);
+
+            // Calculate chart values
+            long length = endTime - startTime;
+
+            XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();
+            series.setName("Temperature Series");
+            List<Long> timestamps = List.of(startTime, startTime + length / 6, startTime + 2 * length / 6, startTime + 3 * length / 6,
+                    startTime + 4 * length / 6, startTime + 5 * length / 6, startTime + length);
+            // Draw chart
+
+            SimpleDateFormat formatter = new SimpleDateFormat("kk:mm");
+            for (long time : timestamps) {
+                try {
+                    WeatherData forecast = weatherPoint.getForecastAtTime(time);
+
+                    //Initialise the data points that we can actually access between values
+                    XYChart.Data<Number, Number> dataPoint = new XYChart.Data<Number, Number>(forecast.getTimeForData(), forecast.getAvgTemperatureCelsius());
+
+
+                    // Add the data
+                    series.getData().add(dataPoint);
+                } catch (ForecastException fe) {
+                    // Truncate the chart if we lose forecasts towards the end
+                    System.err.println("Chart tried to fetch non-existent forecast");
+                }
+            }
+
+            // Set up x Axis
+            xAxis.setLabel("Times");
+            xAxis.setAutoRanging(false);
+            xAxis.setLowerBound(startTime);
+            xAxis.setUpperBound(endTime);
+            xAxis.setTickUnit(length / 6);
+
+            xAxis.setTickLabelFormatter(new StringConverter<Number>() {
+                @Override
+                public String toString(Number n) {
+                    return formatter.format(new Date(n.longValue() * 1000));
+                }
+
+                @Override
+                public Number fromString(String s) {
+                    try {
+                        return formatter.parse(s).getTime() / 1000;
+                    } catch (ParseException e) {
+                        throw new RuntimeException("Failed to parse date");
+                    }
+                }
+            });
+
+            // Set up Y axis
+            linechart.getYAxis().setLabel("Temperatures");
+
+            linechart.getData().addAll(series);
+
+            // Dodgy hack - present the actual points from showing up (the user doesn't need to know)
+            // From this StackOverflow answer https://stackoverflow.com/a/39674496
+            for(XYChart.Data<Number, Number> dataPoint : series.getData()) {
+                StackPane stackPane = (StackPane) dataPoint.getNode();
+                stackPane.setVisible(false);
+            }
+        } catch (ForecastException fe) {
+            // The hike is too far in advance to even get daily values. We should show typicals instead.
+            WeatherData data = api.getTypicalWeatherForPoint(longitude, latitude, startTime);
+
+            //Let the user know that we're showing typical data
+            hikeName.setText(hike.getName() + " (Typical Data)");
+            setWeatherLabels(data);
+
+        }
+    }
+
+    private void setWeatherLabels(WeatherData data) {
         // Convert weather information to strings
-        String humidityString = Math.round(data.getHumidity()*100) + "%";
+        String humidityString = Math.round(data.getHumidity() * 100) + "%";
         String visibilityString = Double.toString(data.getVisibility());
         String pressureString = Double.toString(data.getPressure());
-        String highTempString = Double.toString(data.getHighTemperatureCelsius());
-        String precipProbString = Math.round(data.getPrecipitationProbability()*100) + "%";
+        String tempString = Math.round(data.getAvgTemperatureCelsius()) + "°C";
+        String precipProbString = Math.round(data.getPrecipitationProbability() * 100) + "%";
         String precipIntensityString = Double.toString(data.getPrecipitationIntensity());
 
         // Put strings onto the UI
         humidity.setText(humidityString);
         visibility.setText(visibilityString);
         air_pressure.setText(pressureString);
-        high.setText(highTempString);
+        temperature.setText(tempString);
         probability.setText(precipProbString);
         intensity.setText(precipIntensityString);
-
-        // Calculate chart values
-        long length = endTime - startTime;
-
-        XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();
-        series.setName("Temperature Series");
-        List<Long> timestamps = List.of(startTime, startTime + length / 6, startTime + 2 * length / 6, startTime + 3 * length / 6,
-                startTime + 4 * length / 6, startTime + 5 * length / 6, startTime + length);
-
-        // Draw chart
-
-        SimpleDateFormat formatter = new SimpleDateFormat("kk:mm");
-        for (long time : timestamps) {
-            Date dateTime = new Date(time * 1000);
-
-            series.getData().add(new XYChart.Data<Number, Number>(time, weatherPoint.getForecastAtTime(time).getAvgTemperatureCelsius()));
-        }
-        xAxis.setLabel("Times");
-        xAxis.setTickLabelFormatter(new StringConverter<Number>() {
-            @Override
-            public String toString(Number n) {
-                return formatter.format(new Date(n.longValue()*1000));
-            }
-
-            @Override
-            public Number fromString(String s) {
-                try {
-                    return formatter.parse(s).getTime() / 1000;
-                } catch (ParseException e) {
-                    throw new RuntimeException("Failed to parse date");
-                }
-            }
-        });
-
-        linechart.getYAxis().setLabel("Temperatures");
-        linechart.getData().addAll(series);
-
-        System.out.println(linechart);
     }
 
     public DetailsControl(Hike hike) throws IOException {
